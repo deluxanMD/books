aaf517ddb1f428aea29d33e0349b73c7e69e5a3e
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-01-03 21:26:01 +0530

    describe and it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,16 @@
-import React from "react";
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 
-test("renders <App /> component", () => {
-  render(<App />);
-  const divElement = screen.getByTestId("App.Container");
-  expect(divElement).toBeInTheDocument();
-});
+describe("<App />", () => {
+  it("renders <App /> component", () => {
+    render(<App />);
+    const divElement = screen.getByTestId("App.Container");
+    expect(divElement).toBeInTheDocument();
+  });
 
-test("should contains paragraph", () => {
-  render(<App />);
-  const pElement = screen.getByText(/App Component/i);
-  expect(pElement).toBeInTheDocument();
+  it("should contains paragraph", () => {
+    render(<App />);
+    const pElement = screen.getByText(/App Component/i);
+    expect(pElement).toBeInTheDocument();
+  });
 });
