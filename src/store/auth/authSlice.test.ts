4d8f83e052e0d350ed563da58f4821dacecfb9be
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2023-01-03 23:18:16 +0530

    test for sign in reducer

--- src/store/auth/authSlice.test.ts
+++ src/store/auth/authSlice.test.ts
@@ -1,8 +1,13 @@
-import AuthSlice, {initialState} from "./authSlice";
+import AuthSlice, {initialState, signIn} from "./authSlice";
 
 describe("authSlice", () => {
   test("initialize with initial value", () => {
     const authSliceInit = AuthSlice(initialState, {type: "unknown"});
     expect(authSliceInit).toBe(initialState);
+  });
+
+  test("should handles isLoggedIn property", () => {
+    const authSlice = AuthSlice(initialState, signIn());
+    expect(authSlice.isLoggedIn).toBe(true);
   });
 });
